4d6be3df12dd47d8d28f31ed15be7c5f2038c830
Alonso Bonilla <luis.labp06@gmail.com>
2023-04-26 23:57:34 -0500

    Registering service worker and manifest

--- js/app.js
+++ js/app.js
@@ -1,54 +1,14 @@
-import { datosCita, nuevaCita } from "./functions.js";
-import { mascotaInput, propietarioInput, telefonoInput, fechaInput, horaInput, sintomasInput, formulario} from './selectors.js';
-import { Ui } from "./classes/Ui.js";
-export let DB;
-//Instanciamos un obj de la UI para mostrar las citas
-const ui = new Ui();
-//Registrar eventos
-window.onload = () => {
-    eventListeners();
-    crearDB();
-}
-function eventListeners() {
-    mascotaInput.addEventListener('change', datosCita);
-    propietarioInput.addEventListener('change', datosCita);
-    telefonoInput.addEventListener('change', datosCita);
-    fechaInput.addEventListener('change', datosCita);
-    horaInput.addEventListener('change', datosCita);
-    sintomasInput.addEventListener('change', datosCita);
-
-    formulario.addEventListener('submit', nuevaCita);
+if('serviceWorker' in navigator) {
+    serviceWorker()
+} else {
+    console.log("Service worker no soportado")
 }
 
-function crearDB() {
-    //Crear la base de datos
-    const crearDB = window.indexedDB.open('citas',1);
-    //Si hay error
-    crearDB.onerror = function() {
-        console.log('Hubo un error');
+async function serviceWorker() {
+    try {
+        const registrado = await navigator.serviceWorker.register("./sw.js")
+        console.log("Se instaló correctamente ", registrado)
+    } catch (error) {
+        console.log("Fallo la autenticación..", error)
     }
-    crearDB.onsuccess = function() {
-        console.log('Base de datos creada');
-        DB = crearDB.result;
-        ui.imprimirCitas();
-    }
-    //Definir el schema
-    crearDB.onupgradeneeded = function(e) {
-        const db = e.target.result;
-        const objectStore = db.createObjectStore('citas', {
-            keyPath: 'id',
-            autoIncrement: true
-        })
-        
-        //Definir las columnas
-        objectStore.createIndex('mascota','mascota',{unique:false});
-        objectStore.createIndex('propietario','propietario',{unique:false});
-        objectStore.createIndex('telefono','telefono',{unique:false});
-        objectStore.createIndex('fecha','fecha',{unique:false});
-        objectStore.createIndex('hora','hora',{unique:false});
-        objectStore.createIndex('sintomas','sintomas',{unique:false});
-        objectStore.createIndex('id','id',{unique:true});
-        
-    }
-
 }
